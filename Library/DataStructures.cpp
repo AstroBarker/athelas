eefbf8fecc3413d3c2ef33414a64e92f92a14a0c
Brandon Barker <barker49@msu.edu>
2021-06-11 21:09:30 -0400

    added 3D data structure for conserved fields etc.

--- Library/DataStructures.cpp
+++ Library/DataStructures.cpp
@@ -2,6 +2,7 @@
  * Classes for holding multidimensional data
  **/
 
+
 class DataStructure2D
 {
 public:
@@ -37,3 +38,47 @@ double DataStructure2D::operator()(unsigned int i, unsigned int j) const
 {
   return Data[i * Cols + j];
 }
+
+
+// This will be e.g., conserved variables
+class DataStructure3D
+{
+
+public:
+  DataStructure3D( unsigned int N1, unsigned int N2, unsigned int N3 );
+  double& operator()( unsigned int i, unsigned int j, unsigned int k );
+  double operator()( unsigned int i, unsigned int j, unsigned int k ) const;
+
+  ~DataStructure3D()
+  {
+    delete [] Data;
+  }
+
+private:
+  unsigned int Size1;
+  unsigned int Size2;
+  unsigned int Size3;
+  double* Data;
+
+};
+
+// init as {nNodes, nX, nCF}
+DataStructure3D::DataStructure3D( unsigned int N1, unsigned int N2, unsigned int N3 )
+{
+  Size1 = N1;
+  Size2 = N2;
+  Size3 = N3;
+
+  Data = new double[N1 * N2 * N3];
+}
+
+double& DataStructure3D::operator()( unsigned int i, unsigned int j, unsigned int k )
+{
+  return Data[i + Size2*j + Size2*Size3*k];
+}
+
+double DataStructure3D::operator()( unsigned int i, unsigned int j, unsigned int k ) const
+{                                                
+  return Data[i + Size2*j + Size2*Size3*k];      
+}
+
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
  - [ ] Start with Lagrange and Legendre polynomial bases
     - [ ] We need to put in LG (and LGL) quadratures.
 - [ ] Make directory for test problem and setup
+- [ ] Use LAPACKE for lapack calls (more portable)
