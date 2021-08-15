874669b02f31a788164b460cce44275a6a1d5e65
Brandon Barker <barker49@msu.edu>
2021-08-15 13:35:35 -0400

    drafted initialize Sod. next is an Output routine...

--- Example/Hydro/Initialization.cpp
+++ Example/Hydro/Initialization.cpp
@@ -18,7 +18,7 @@ int main( int argc, char* argv[] )
   const std::string ProblemName = "Sod";
 
   const unsigned int nX     = 100;
-  const unsigned int nNodes = 3;
+  const unsigned int nNodes = 2;
 
   const unsigned int nGuard = 1;
 
@@ -36,16 +36,50 @@ int main( int argc, char* argv[] )
   // We may need more allocations later. Put them here.
 
   // Initialize fields
-  InitializeFields( uCF, Grid, ProblemName );
+  InitializeFields( uCF, uPF, Grid, ProblemName );
 }
 
-void InitializeFields( DataStructure3D& uCF, GridStructure& Grid, const std::string ProblemName )
+void InitializeFields( DataStructure3D& uCF, DataStructure3D& uPF, GridStructure& Grid, const std::string ProblemName )
 {
-  std::cout << ProblemName << std::endl;
+  std::cout << " === Initializing: " << ProblemName << std::endl;
+
+  const unsigned int ilo    = Grid.Get_ilo();
+  const unsigned int ihi    = Grid.Get_ihi();
+  const unsigned int nNodes = Grid.Get_nNodes();
+
+  const unsigned int iCF_Tau = 0;
+  const unsigned int iCF_V   = 1;
+  const unsigned int iCF_E   = 2;
+
+  const unsigned int iPF_D = 0;
 
   if ( ProblemName == "Sod" )
   {
-    std::cout << "sod :) " << std::endl;
+    const double V0  = 0.0;
+    const double D_L = 1.0;
+    const double D_R = 0.125;
+    const double P_L = 1.0;
+    const double P_R = 0.1;
+
+    double X1 = 0.0;
+    for ( unsigned int iX = ilo; iX <= ihi; iX++ )
+    for ( unsigned int iNodeX = 0; iNodeX < nNodes; iNodeX++  )
+    {
+      X1 = Grid.NodeCoordinate( iX, iNodeX );
+
+      if ( X1 <= 0.5 )
+      {
+        uCF(iNodeX, iX, iCF_Tau) = 1.0 / D_L;
+        uCF(iNodeX, iX, iCF_V)   = V0;
+        uCF(iNodeX, iX, iCF_E)   = (P_L / 0.4) * uCF(iNodeX, iX, iCF_Tau);
+        uPF(iNodeX, iX, iPF_D)   = D_L;
+      }else{
+        uCF(iNodeX, iX, iCF_Tau) = 1.0 / D_R;
+        uCF(iNodeX, iX, iCF_V)   = V0;
+        uCF(iNodeX, iX, iCF_E)   = (P_R / 0.4) * uCF(iNodeX, iX, iCF_Tau);
+        uPF(iNodeX, iX, iPF_D)   = D_R;
+      }
+    }
   }
   else{
     std::cerr << "Please choose a valid ProblemName";
--- Example/Hydro/Initialization.h
+++ Example/Hydro/Initialization.h
@@ -1,6 +1,6 @@
 #ifndef INITIALIZATION_H
 #define INITIALIZATION_H
 
-void InitializeFields( DataStructure3D& uCF, GridStructure& Grid, const std::string ProblemName );
+void InitializeFields( DataStructure3D& uCF, DataStructure3D& uPF, GridStructure& Grid, const std::string ProblemName );
 
 #endif
