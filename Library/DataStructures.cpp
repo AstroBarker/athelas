5154295d9e89a53b47d918b5c0ee905066e4e47b
Brandon Barker <barker49@msu.edu>
2021-08-16 13:07:54 -0400

    reorganized DataStrcture3D

--- Library/DataStructures.cpp
+++ Library/DataStructures.cpp
@@ -22,28 +22,29 @@ double DataStructure2D::operator()(unsigned int i, unsigned int j) const
   return Data[i * Cols + j];
 }
 
-// init as {nNodes, nX, nCF}
+// init as {nCF, nX, nNodes}
 DataStructure3D::DataStructure3D( unsigned int N1, unsigned int N2, unsigned int N3 )
 {
-  Size1 = N1;
-  Size2 = N2;
-  Size3 = N3;
+  Size1 = N1; // nCF
+  Size2 = N2; // nX
+  Size3 = N3; // nNodes
 
   mSize = Size1 * Size2 * Size3;
 
   Data = new double[mSize];
 }
 
+// access (iCF, iX, iN)
 double& DataStructure3D::operator()
   ( unsigned int i, unsigned int j, unsigned int k )
 {
-  return Data[i + Size2*j + Size2*Size3*k];
+  return Data[k + j*Size3 + i * Size3 * Size2];
 }
 
 double DataStructure3D::operator()
   ( unsigned int i, unsigned int j, unsigned int k ) const
 {                                                
-  return Data[i + Size2*j + Size2*Size3*k];      
+  return Data[k + j*Size3 + i * Size3 * Size2];      
 }
 
 // Copy Grid contents into new array
