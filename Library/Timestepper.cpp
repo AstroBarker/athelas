7b3a8d1f3420325733bf613497538defad4d6ad1
Brandon Barker <barker49@msu.edu>
2021-08-26 00:17:31 -0400

    work on SSPRK timestepper

---
+++ Library/Timestepper.cpp
@@ -0,0 +1,119 @@
+/**
+ * File     :  Timestepper.cpp.cpp
+ * --------------
+ *
+ * Author   : Brandon L. Barker
+ * Purpose  : SSPRK timestepping routines
+**/ 
+
+#include <iostream>
+#include <vector>
+
+#include "Grid.h"
+#include "DataStructures.h"
+#include "Timestepper.h"
+#include "Fluid_Discretization.h"
+
+// Initialize arrays for timestepper
+void InitializeTimestepper( const unsigned short int nStages, 
+  const int nX, const unsigned int nN,
+  DataStructure2D& a_jk, DataStructure2D& b_jk,
+  std::vector<DataStructure3D> U_s, std::vector<DataStructure3D> dU_s )
+{
+
+  if ( nStages == 1 )
+  {
+    a_jk(0,0) = 1.0;
+    b_jk(0,0) = 1.0;
+  }
+  else if ( nStages == 2 )
+  {
+    a_jk(0,0) = 1.0;
+    a_jk(1,0) = 0.5;
+    a_jk(1,1) = 0.5;
+
+    b_jk(0,0) = 1.0;
+    b_jk(1,0) = 0.0;
+    b_jk(1,1) = 0.5;
+  }
+  else if ( nStages == 3 )
+  {
+    a_jk(0,0) = 1.0;
+    a_jk(1,0) = 0.75;
+    a_jk(1,1) = 0.25;
+    a_jk(2,0) = 1.0 / 3.0;
+    a_jk(2,1) = 0.0;
+    a_jk(2,2) = 2.0 / 3.0;
+
+    b_jk(0,0) = 1.0;
+    b_jk(1,0) = 0.0;
+    b_jk(1,1) = 0.25;
+    b_jk(2,0) = 0.0;
+    b_jk(2,1) = 0.0;
+    b_jk(2,2) = 2.0 / 3.0;
+  }
+  else
+  {
+    std::cerr << "\n === Please enter an appropriate number of SSPRK stages (1,2,3) === \n" << std::endl;
+    exit(-1);
+  }
+  
+}
+
+
+/** 
+ * Update Solution with SSPRK methods 
+ * TODO: adjust for spherically symmetric
+ * NOTE: `work` forms inner sum (3D) for timestep and Flux_q(3D) for increment
+**/
+
+void UpdateFluid( myFuncType ComputeIncrement, double dt, 
+  DataStructure3D& U, GridStructure& Grid, 
+  DataStructure2D& a_jk, DataStructure2D& b_jk,
+  std::vector<DataStructure3D> U_s, std::vector<DataStructure3D> dU_s,
+  DataStructure3D& dU, DataStructure3D& work, DataStructure2D& dFlux_num, 
+  DataStructure2D& uCF_F_L, DataStructure2D& uCF_F_R, std::vector<double> Flux_U, 
+  std::vector<double> Flux_P, std::vector<double> uCF_L, std::vector<double> uCF_R,
+  const short unsigned int nStages, const std::string BC )
+{
+
+  const unsigned int nNodes = Grid.Get_nNodes();
+  const unsigned int ilo    = Grid.Get_ilo();
+  const unsigned int ihi    = Grid.Get_ihi();
+
+  unsigned short int i;
+
+  U_s[0] = U;
+  
+  for ( unsigned short int iS = 1; iS <= nStages; iS++ )
+  {
+    i = iS - 1;
+    // re-zero the summation variable `work`
+    work.zero();
+
+    // --- Inner update loop ---
+    
+    for ( unsigned int j = 0; j < iS; j++ )
+    {
+      ComputeIncrement( U_s[j], Grid, dU_s[j], work, dFlux_num, uCF_F_L, uCF_F_R, 
+                        Flux_U, Flux_P, uCF_L, uCF_R, BC );
+
+      work.zero(); // reset work for summation
+
+      // inner sum
+      for ( unsigned int iCF = 0; iCF < 3; iCF++ )
+      for ( unsigned int iX = ilo; iX <= ihi; iX++ )
+      for ( unsigned int iN = 0; iN < nNodes; iN++ )
+      {
+        work(iCF,iX,iN) += a_jk(i,j) * U_s[j](iCF,iX,iN) 
+                        + dt * b_jk(i,j) * dU_s[j](iCF,iX,iN);
+      }
+      
+      U_s[i+1] = work; // i = 0.. overwrite initial.. okay?
+      // TODO: ApplySlopeLimiter
+    }
+  }
+
+  U = U_s[nStages-1];
+
+}
---
+++ Library/Timestepper.h
@@ -0,0 +1,24 @@
+#ifndef TIMESTEPPER_H
+#define TIMESTEPPER_H
+
+typedef void myFuncType (DataStructure3D&, GridStructure&, 
+  DataStructure3D&, DataStructure3D&, DataStructure2D&, 
+  DataStructure2D&, DataStructure2D&, std::vector<double>, 
+  std::vector<double>, std::vector<double>, std::vector<double>,
+  const std::string);
+
+void InitializeTimestepper( const unsigned short int nStages, 
+  const int nX, const unsigned int nN,
+  DataStructure2D& a_jk, DataStructure2D& b_jk,
+  std::vector<DataStructure3D> U_s, std::vector<DataStructure3D> dU_s );
+
+void UpdateFluid( myFuncType ComputeIncrement, double dt, 
+  DataStructure3D& U, GridStructure& Grid, 
+  DataStructure2D& a_jk, DataStructure2D& b_jk,
+  std::vector<DataStructure3D> U_s, std::vector<DataStructure3D> dU_s,
+  DataStructure3D& dU, DataStructure3D& Flux_q, DataStructure2D& dFlux_num, 
+  DataStructure2D& uCF_F_L, DataStructure2D& uCF_F_R, std::vector<double> Flux_U, 
+  std::vector<double> Flux_R, std::vector<double> uCF_L, std::vector<double> uCF_R,
+  const short unsigned int nStages, const std::string BC );
+
+#endif
