72d4c1085f2bc8e2c38f37d886c76f41ba63c52b
Brandon Barker <barker49@msu.edu>
2021-08-25 23:52:56 -0400

    added error calling class

---
+++ Library/Error.h
@@ -0,0 +1,23 @@
+/**
+ * File    :  Error.h
+ * --------------
+ *
+ * Author  : Brandon L. Barker
+ * Purpose : Print error messages ...
+**/  
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stdexcept>
+
+class Error : public std::runtime_error
+{
+
+  public :
+
+  Error( const std::string& message ) : std::runtime_error( message ) {}
+
+};
+
+#endif
