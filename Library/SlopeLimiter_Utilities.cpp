b2b34ac3c0f1570d16d87413028d65e581b7e6b5
Brandon Barker <bbarker.py@protonmail.ch>
2021-12-29 22:42:53 -0500

    redrafted barth-jespersen

--- Library/SlopeLimiter_Utilities.cpp
+++ Library/SlopeLimiter_Utilities.cpp
@@ -58,23 +58,21 @@ double minmodB( double a, double b, double c, double dx, double M )
  *  Barth-Jespersen limiter
  *  Parameters:
  *  -----------
- *  U_vertex: array of vertex values on given cell
- *  U_c: array of cell averages of current cell + neighbors 
- *    [ left, current, right ]
+ *  U_v_*: left/right vertex values on target cell
+ *  U_c_*: cell averages of target cell + neighbors 
+ *    [ left, target, right ]
  *  alpha: scaling coefficient for BJ limiter. 
  *    alpha=1 is classical limiter, alpha=0 enforces constant solutions
 **/
-double BarthJespersen( double* U_vertex, double* U_c, double alpha )
+double BarthJespersen( double U_v_L, double U_v_R, double U_c_L, 
+  double U_c_T, double U_c_R, double alpha )
 {
   // Get U_min, U_max
-  double U_min = 10000000.0 * U_c[1];
-  double U_max = std::numeric_limits<double>::epsilon() * U_c[1];
+  double U_min = 10000000.0 * U_c_T;
+  double U_max = std::numeric_limits<double>::epsilon() * U_c_T;
 
-  for( unsigned int i = 0; i < 3; i++ )
-  {
-    U_min = std::min( U_min, U_c[i] );
-    U_max = std::max( U_max, U_c[i] );
-  }
+  U_min = std::min( U_min, std::min( U_c_T, std::min( U_c_L, U_c_R ) ) );
+  U_max = std::max( U_max, std::max( U_c_T, std::max( U_c_L, U_c_R ) ) );
 
   // loop of cell certices
   double phi = 10000000000.0;
@@ -82,31 +80,31 @@ double BarthJespersen( double* U_vertex, double* U_c, double alpha )
   double phi_R = 0.0;
 
   // left vertex
-  if ( U_vertex[0] - U_c[1] > 0.0 )
+  if ( U_v_L - U_c_T > 0.0 )
   {
-    phi_L = std::min( 1.0, alpha * (U_max - U_c[1]) / (U_vertex[0] - U_c[1]) ); 
+    phi_L = std::min( 1.0, alpha * (U_max - U_c_T) / (U_v_L - U_c_T) ); 
   }
-  else if ( U_vertex[0] - U_c[1] == 0.0 )
+  else if ( U_v_L - U_c_T == 0.0 )
   {
     phi_L = 1.0;
   }
   else
   {  
-    phi_L = std::min( 1.0, alpha * (U_min - U_c[1]) / (U_vertex[0] - U_c[1]) ); 
+    phi_L = std::min( 1.0, alpha * (U_min - U_c_T) / (U_v_L - U_c_T) ); 
   }
 
   // right vertex
-  if ( U_vertex[1] - U_c[1] > 0.0 )
+  if ( U_v_R - U_c_T > 0.0 )
   {
-    phi_R = std::min( 1.0, alpha * (U_max - U_c[1]) / (U_vertex[1] - U_c[1]) ); 
+    phi_R = std::min( 1.0, alpha * (U_max - U_c_T) / (U_v_R - U_c_T) ); 
   }
-  else if ( U_vertex[1] - U_c[1] == 0.0 )
+  else if ( U_v_R - U_c_T == 0.0 )
   {
     phi_R = 1.0;
   }
   else
   {  
-    phi_R = std::min( 1.0, alpha * (U_min - U_c[1]) / (U_vertex[1] - U_c[1]) ); 
+    phi_R = std::min( 1.0, alpha * (U_min - U_c_T) / (U_v_R - U_c_T) ); 
   }
 
   // return min of two values
--- Library/SlopeLimiter_Utilities.h
+++ Library/SlopeLimiter_Utilities.h
@@ -3,5 +3,6 @@
 
 double minmod( double a, double b, double c );
 double minmodB( double a, double b, double c, double dx, double M );
-double BarthJespersen( double* U_vertex, double* U_c, double alpha );
+double BarthJespersen( double U_v_L, double U_v_R, double U_c_L, 
+  double U_c_T, double U_c_R, double alpha );
 #endif
