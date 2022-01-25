184760f8be4ab8d366288848666d92f22cf3f189
Brandon Barker <bbarker.py@protonmail.ch>
2022-01-25 18:20:54 -0500

    Updated timestep calculation

--- Library/FluidUtilities.h
+++ Library/FluidUtilities.h
@@ -7,9 +7,9 @@ double Flux_Fluid( double V, double P, unsigned int iCF );
 double Fluid( double Tau, double V, double Em_T, int iCF );
 void NumericalFlux_Gudonov( double vL, double vR, double pL, double pR, 
      double zL, double zR, double& Flux_U, double& Flux_P  );
-void NumericalFlux_HLL( double tauL, double tauR, double vL, double vR, 
-  double eL, double eR, double pL, double pR, double zL, double zR, 
-  int iCF, double& out );
+void NumericalFlux_HLLC( double vL, double vR, double pL, double pR, 
+  double cL, double cR, double rhoL, double rhoR, 
+  double& Flux_U, double& Flux_P  );
 double ComputeTimestep_Fluid( DataStructure3D& U, 
      GridStructure& Grid, const double CFL );
 
