d6d4a5214d690285b5298ba0fc9630ddbc203caa
Brandon Barker <bbarker.py@protonmail.ch>
2022-06-12 12:59:43 -0400

    reset default GAMMA

--- Library/EquationOfStateLibrary_IDEAL.cpp
+++ Library/EquationOfStateLibrary_IDEAL.cpp
@@ -9,13 +9,13 @@
 #include <math.h> /* sqrt */
 
 // Compute pressure assuming an ideal gas
-double ComputePressureFromPrimitive_IDEAL( double Ev, double GAMMA = 5.0 / 3.0 )
+double ComputePressureFromPrimitive_IDEAL( double Ev, double GAMMA = 1.4 )
 {
   return ( GAMMA - 1.0 ) * Ev;
 }
 
 double ComputePressureFromConserved_IDEAL( double Tau, double V, double Em_T,
-                                           double GAMMA = 5.0 / 3.0 )
+                                           double GAMMA = 1.4 )
 {
   double Em = Em_T - 0.5 * V * V;
   double Ev = Em / Tau;
@@ -25,7 +25,7 @@ double ComputePressureFromConserved_IDEAL( double Tau, double V, double Em_T,
 }
 
 double ComputeSoundSpeedFromConserved_IDEAL( double Tau, double V, double Em_T,
-                                             double GAMMA = 5.0 / 3.0 )
+                                             double GAMMA = 1.4 )
 {
   double Em = Em_T - 0.5 * V * V;
 
--- Library/EquationOfStateLibrary_IDEAL.h
+++ Library/EquationOfStateLibrary_IDEAL.h
@@ -1,7 +1,8 @@
 #ifndef EQUATIONOFSTATELIBRARY_IDEAL_H
 #define EQUATIONOFSTATELIBRARY_IDEAL_H
 
-double ComputePressureFromPrimitive_IDEAL( double Ev, double GAMMA = 1.4 );
+double ComputePressureFromPrimitive_IDEAL( double Ev,
+                                           double GAMMA = 1.4 );
 double ComputePressureFromConserved_IDEAL( double Tau, double V, double Em_T,
                                            double GAMMA = 1.4 );
 double ComputeSoundSpeedFromConserved_IDEAL( double Tau, double V, double Em_T,
