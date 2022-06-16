45cd8827fd7c15dc6298a353be35c7524b6ddec6
Brandon Barker <bbarker.py@protonmail.ch>
2022-06-16 12:44:17 -0400

    some internal energy routines

--- Library/EquationOfStateLibrary_IDEAL.h
+++ Library/EquationOfStateLibrary_IDEAL.h
@@ -7,5 +7,8 @@ double ComputePressureFromConserved_IDEAL( double Tau, double V, double Em_T,
                                            double GAMMA = 1.4 );
 double ComputeSoundSpeedFromConserved_IDEAL( double Tau, double V, double Em_T,
                                              double GAMMA = 1.4 );
+double ComputeInternalEnergy( Kokkos::View<double***> U, const ModalBasis& Basis, 
+                              const unsigned int iX, const unsigned int iN );
+double ComputeInternalEnergy( Kokkos::View<double***> U, const unsigned int iX );
 
 #endif
