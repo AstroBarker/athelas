2beec3fe28c9cff07c8b68f3e32834ad74097560
Brandon Barker <bbarker.py@protonmail.ch>
2024-03-13 23:02:06 -0600

    feat: python class placeholder

---
+++ tools/python/athelas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+class Athelas:
+  """
+  Athelas Python class
+  """
+
+  def __init__(self, fn):
+    print(fn)
+
+  def __str__(self):
+    print(" --- Athelas Parameters ---")
+    return ""
+
+
+if __name__ == "__main__":
+  x = 0
