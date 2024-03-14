bd2a30fa0aa7e6ac1b2cd1e97786619b90e08a55
Brandon Barker <bbarker.py@protonmail.ch>
2024-03-14 23:55:57 +0000

    py

--- tools/python/athelas.py
+++ tools/python/athelas.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import h5py
 
 class Athelas:
   """
@@ -12,6 +14,11 @@ class Athelas:
   def __str__(self):
     print(" --- Athelas Parameters ---")
     return ""
+
+  def load_(self, fn):
+    """
+    load athelas output
+    """
 
 
 if __name__ == "__main__":
