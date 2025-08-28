b9554612bda015607732add0134d851f6ed26bbc
Brandon Barker <bbarker.py@protonmail.ch>
2025-08-28 14:09:43 -0600

    wip

---
+++ src/pgen/one_zone_ionization.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cmath>
+
+#include "basis/polynomial_basis.hpp"
+#include "eos/eos_variant.hpp"
+#include "geometry/grid.hpp"
+#include "state/state.hpp"
+#include "utils/abstractions.hpp"
+
+/**
+ * Initialize one_zone_ionization test
+ **/
+void one_zone_ionization_init(State* state, GridStructure* grid, ProblemIn* pin,
+                          const EOS* /*eos*/,
+                          ModalBasis* /*fluid_basis = nullptr*/,
+                          ModalBasis* /*radiation_basis = nullptr*/) {
+  if (pin->param()->get<std::string>("eos.type") != "ideal") {
+    THROW_ATHELAS_ERROR("One zone ionization requires ideal gas eos!");
+  }
+
+  View3D<double> uCF = state->u_cf();
+  View3D<double> uPF = state->u_pf();
+
+  const int ilo = 1;
+  const int ihi = grid->get_ihi();
+  const int nNodes = grid->get_n_nodes();
+
+  const int q_Tau = 0;
+  const int q_V = 1;
+  const int q_E = 2;
+
+  const int iPF_D = 0;
+
+  const int iCR_E = 3;
+
+  const auto V0 = pin->param()->get<double>("problem.params.v0", 0.0);
+  const auto logD = pin->param()->get<double>("problem.params.logrho", -7.0);
+  const auto logE_gas =
+      pin->param()->get<double>("problem.params.logE_gas", 10.0);
+  const auto logE_rad =
+      pin->param()->get<double>("problem.params.logE_rad", 12.0);
+
+  const double D = std::pow(10.0, logD);
+  const double Ev_gas = std::pow(10.0, logE_gas);
+  const double Ev_rad = std::pow(10.0, logE_rad);
+
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<>(0, ihi + 2), KOKKOS_LAMBDA(int ix) {
+        const int k = 0;
+
+        uCF(ix, k, q_Tau) = 1.0 / D;
+        uCF(ix, k, q_V) = V0;
+        uCF(ix, k, q_E) = Ev_gas / D;
+        uCF(ix, k, iCR_E) = Ev_rad;
+
+        for (int iNodeX = 0; iNodeX < nNodes; iNodeX++) {
+          uPF(ix, iNodeX, iPF_D) = D;
+        }
+      });
+
+  // Fill density in guard cells
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<>(0, ilo), KOKKOS_LAMBDA(int ix) {
+        for (int iN = 0; iN < nNodes; iN++) {
+          uPF(ilo - 1 - ix, iN, 0) = uPF(ilo + ix, nNodes - iN - 1, 0);
+          uPF(ilo + 1 + ix, iN, 0) = uPF(ilo - ix, nNodes - iN - 1, 0);
+        }
+      });
+}
