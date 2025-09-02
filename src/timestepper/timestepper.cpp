8e8672cae26000e306cc3848e32c9a886bfb5de0
Brandon Barker <bbarker.py@protonmail.ch>
2025-09-02 14:05:09 -0600

    timestepper:: dont allocate dU_implicit if not needed

--- src/timestepper/timestepper.cpp
+++ src/timestepper/timestepper.cpp
@@ -28,14 +28,18 @@ TimeStepper::TimeStepper(const ProblemIn* pin, GridStructure* grid, EOS* eos)
       dU_s_("dU_s", nStages_ + 1, mSize_ + 1,
             pin->param()->get<int>("fluid.porder"),
             3 + (pin->param()->get<bool>("physics.rad_active")) * 2),
-      dU_s_implicit_("dU_s_implicit", nStages_ + 1, mSize_ + 1,
-                     pin->param()->get<int>("fluid.porder"),
-                     5), // TODO(astrobarker) : neednt allocate
       SumVar_U_("SumVar_U", mSize_ + 1, pin->param()->get<int>("fluid.porder"),
                 3 + (pin->param()->get<bool>("physics.rad_active")) * 2),
       grid_s_(nStages_ + 1, GridStructure(pin)),
       stage_data_("StageData", nStages_ + 1, mSize_ + 1),
-      flux_u_("flux_u_", nStages_ + 1, mSize_ + 1), eos_(eos) {}
+      flux_u_("flux_u_", nStages_ + 1, mSize_ + 1), eos_(eos) {
+
+  if (integrator_.method == MethodType::IM ||
+      integrator_.method == MethodType::IMEX) {
+    dU_s_implicit_ = View4D<double>("dU_s_implicit", nStages_ + 1, mSize_ + 1,
+                                    pin->param()->get<int>("fluid.porder"), 5);
+  }
+}
 
 [[nodiscard]] auto TimeStepper::get_n_stages() const noexcept -> int {
   return integrator_.num_stages;
