472afafb8b1c577ef8030206ecb37c9c00f10751
Brandon Barker <bbarker.py@protonmail.ch>
2025-09-30 13:11:22 -0600

    add maybe_unused to signal handlers

--- src/main.cpp
+++ src/main.cpp
@@ -36,10 +36,10 @@ auto main(int argc, char **argv) -> int {
   std::string input_path = *input_result;
 
 #ifdef ATHELAS_DEBUG
-  feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
-  auto sig1 = signal(SIGSEGV, segfault_handler);
-  auto sig2 = signal(SIGABRT, segfault_handler);
-  auto sig3 = signal(SIGFPE, segfault_handler);
+  feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW);
+  [[maybe_unused]] auto sig1 = signal(SIGSEGV, segfault_handler);
+  [[maybe_unused]] auto sig2 = signal(SIGABRT, segfault_handler);
+  [[maybe_unused]] auto sig3 = signal(SIGFPE, segfault_handler);
 #endif
 
   std::println("# ----------------------------------------------------------");
